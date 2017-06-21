320794dc95916c3a5a604457261da47337a425cc
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-06-21 12:25:13 -0700

    Added Security Group.

---
+++ vpc.tf
@@ -0,0 +1,42 @@
+/*
+  Define security group controlling inbound and oubound access to
+  Kubernetes API servers (masters)
+
+*/
+resource "aws_security_group" "kubernetes-master" {
+
+  name = "kubernetes-master-${var.name}"
+
+  description = "Define inbound and outbound traffic for Kubernetes API server nodes"
+
+  /* link to the correct VPC */
+  vpc_id = "${var.vpc_id}"
+
+  /*
+    Define ingress rules
+  */
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    #cidr_blocks = [ "${var.ip_ranges}" ]
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  /* 
+    Define egress rules
+  */
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags {
+    builtWidth        = "terraform"
+    KubernetesCluster = "${var.name}"
+    Name              = "kubernetes-master-${var.name}"
+  }
+
+}
