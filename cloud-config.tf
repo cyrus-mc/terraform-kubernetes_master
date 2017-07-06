d89c2c2ac0bd541e00a969840a6ec7ee633cabe3
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-07-06 13:44:08 -0700

    Set etcd servers based off whether route53 support is enabled.

--- cloud-config.tf
+++ cloud-config.tf
@@ -2,6 +2,7 @@
   Create user data for each master/etcd instance
 */
 data "template_file" "cloud-config" {
+
   count    =  "${var.servers}"
 
   template = "${file("${path.module}/cloud-config.yml")}"
@@ -13,7 +14,7 @@ data "template_file" "cloud-config" {
 
     HOSTNAME         = "etcd-${count.index + 1}"
 
-    /* the SRV domain for etcd bootstrapping */
+    /* the SRV domain for etcd bootstrapping (only relevant with route53 support) */
     SRV-DOMAIN       = "${var.name}.${var.internal-tld}"
 
     CLUSTER_NAME     = "${var.name}"
@@ -29,7 +30,7 @@ data "template_file" "cloud-config" {
 
     ADVERTISE_IP     = "${element(null_resource.etcd_instance_ip.*.triggers.private_ip, count.index)}"
 
-    ETCD_SERVERS     = "${jsonencode(null_resource.etcd_instance_ip.*.triggers.private_ip)}"
+    ETCD_SERVERS     = "${element(list(jsonencode(null_resource.etcd_instance_ip.*.triggers.private_ip), jsonencode("etcd.${var.name}.${var.internal-tld}")), var.enable_route53)}"
 
     NETWORK_PLUGIN   = "cni"
 
@@ -37,5 +38,7 @@ data "template_file" "cloud-config" {
 
     SERVICE_IP_RANGE = "${var.service_ip_range}"
     DNS_SERVICE_IP   = "${cidrhost(var.service_ip_range, 10)}"
+
   }
+
 }
