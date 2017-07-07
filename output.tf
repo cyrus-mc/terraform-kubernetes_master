88e37051eb70cbfcbe5fe20d5578cf9730b34e70
Matthew Ceroni <matthew.ceroni@smarsh.com>
2017-07-07 13:46:25 -0700

    Added outputs for API and etcd ELB CNAME.

--- output.tf
+++ output.tf
@@ -2,6 +2,14 @@ output "api_elb" {
   value = "${aws_elb.api-internal.dns_name}"
 }
 
+output "api_elb_cname" {
+  value = "${aws_route53_record.CNAME-apiserver.fqdn}"
+}
+
 output "etcd_elb" {
   value = "${aws_elb.etcd-internal.dns_name}"
+}
+
+output "etcd_elb_cname" {
+  value = "${aws_route53_record.CNAME-etcd.fqdn}"
 }
