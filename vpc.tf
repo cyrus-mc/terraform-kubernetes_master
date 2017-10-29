96428f5afa2f3ea135d3c2b832b580393e03ab5e
Matthew Ceroni <matthew.ceroni@dat.com>
2017-10-29 12:25:29 -0700

    Separated security group rules into resources.

--- vpc.tf
+++ vpc.tf
@@ -12,30 +12,45 @@ resource "aws_security_group" "kubernetes-master" {
   /* link to the correct VPC */
   vpc_id = "${element(data.aws_subnet.selected.*.vpc_id, 0)}"
 
-  /*
-    Define ingress rules
-  */
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    #cidr_blocks = [ "${var.ip_ranges}" ]
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
-  /*
-    Define egress rules
-  */
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
-
   /* tag the resource */
   tags = "${merge(local.tags,
                   map("Name", format("kubernetes.%s", var.name)),
                   var.tags)}"
+
+}
+
+resource "aws_security_group_rule" "ingress" {
+
+  /* this is an ingress security rule */
+  type = "ingress"
+
+  /* specify port range and protocol that is allowed */
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  /* specify the allowed CIDR block */
+  cidr_blocks = [ "0.0.0.0/0" ]
+
+  /* link to the above created security group */
+  security_group_id = "${aws_security_group.kubernetes-master.id}"
+
+}
+
+resource "aws_security_group_rule" "egress" {
+
+  /* this is an egress security rule */
+  type = "egress"
+
+  /* specify the port range and protocol that is allowed */
+  from_port = 0
+  to_port   = 0
+  protocol  = "-1"
+
+  /* specify the allowed CIDR block */
+  cidr_blocks = [ "0.0.0.0/0" ]
+
+  /* link to the above created security group */
+  security_group_id = "${aws_security_group.kubernetes-master.id}"
 
 }
